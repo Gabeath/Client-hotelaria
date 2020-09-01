db6ce5ab8f626b81174615e75bfced3164618fd1
Angelo <angelo-sc1@hotmail.com>
2020-09-01 18:15:49 -0300

    validação de alguns campos

--- src/components/FormularioDeReserva.js
+++ src/components/FormularioDeReserva.js
@@ -14,6 +14,10 @@ function FormularioDeReserva(props) {
 
     const [cepValue, setCep] = useState('')
     const [cpfPassaporte, setCPF_Passaporte] = useState('')
+    const [ufs, setUfs] = useState([])
+    const [cidades, setCidades] = useState([])
+    const [ufSelecionada, setUfSelecionada] = useState("")
+    const [cidadeSelecionada, setCidadeSelecionada] = useState("")
     const history = useHistory();
 
     useEffect(() => {
@@ -21,6 +25,37 @@ function FormularioDeReserva(props) {
             dadosIniciaisDaReserva = props.props.props.history.location.state;
         }
     });
+
+    useEffect(() => {
+        async function buscarUFS(){
+            const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            const ufs = await response.json()
+            setUfs(ufs)
+        }
+
+        buscarUFS()
+        
+    },[]);
+
+    useEffect(() => {
+        async function buscarCidades(){
+
+            const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSelecionada}/municipios`)
+            const cidades = await response.json()
+            setCidades(cidades)
+
+            const comboboxUF = document.getElementById('estado')
+            const comboboxCity = document.getElementById('cidade')
+
+            if(comboboxUF.value !== "")
+                comboboxCity.disabled = false
+            else
+                comboboxCity.disabled = true
+        }
+
+        buscarCidades()
+        
+    },[ufSelecionada]);
 
     function handleChange(event, id) {
         if (id === "cep") {
@@ -53,15 +88,14 @@ function FormularioDeReserva(props) {
 
                 //Preenche os campos com "..." enquanto consulta webservice.
                 document.getElementById('logradouro').value = "...";
-                document.getElementById('cidade').value = "...";
-                document.getElementById('estado').value = "...";
 
                 fetch(`https://viacep.com.br/ws/${cep}/json/`)
                     .then((res) => res.json())
-                    .then((data) => {
+                    .then(async (data) => {
                         document.getElementById('logradouro').value = data.logradouro;
-                        document.getElementById('cidade').value = data.localidade;
-                        document.getElementById('estado').value = data.uf;
+                        setUfSelecionada(data.uf)
+                        setCidadeSelecionada(data.localidade)
+
 
                         if (data.logradouro === undefined || data.localidade === undefined || data.uf === undefined) {
                             limpa_formulário_cep();
@@ -108,16 +142,74 @@ function FormularioDeReserva(props) {
         return campoTexto
     }
 
+    function mascararTelefone(e){
+        e.preventDefault()
+
+        let digito = parseInt(e.key)
+        
+        if(!digito)
+            return
+        
+        let tel = document.getElementById("telefone")
+
+        if(tel.value.length == 0){
+            tel.value = "(" + e.key
+            return
+        }
+
+        if(tel.value.length == 3){
+            tel.value += ")" + e.key
+            return
+        }
+
+        if(tel.value.length == 8){
+            tel.value += "-" + e.key
+            return
+        }
+
+        if(tel.value.length == 13){
+            let substringinicio = tel.value.substring(0,8)
+            let digito = tel.value.charAt(9)
+            let substringFim = tel.value.substring(10)
+            tel.value = substringinicio + digito + "-" + substringFim + e.key
+            return
+        }
+
+        if(tel.value.length < 14)
+            tel.value += e.key
+        
+    }
+
     const confirmarReserva = () => {
         carregando(true);
 
         if (document.getElementById("nome").value === "" || document.getElementById("cpfNumPassaporte").value === "" ||
             document.getElementById("cep").value === "" || document.getElementById("logradouro").value === "" ||
-            document.getElementById("numero").value === "" || document.getElementById("cidade").value === "" ||
-            document.getElementById("estado").value === "" || document.getElementById("telefone").value === "") {
+            document.getElementById("numero").value === "" || cidadeSelecionada === "" ||
+            ufSelecionada === "" || document.getElementById("telefone").value === "") {
             carregando(false);
             alert("Preencha todos os campos obrigatórios, sinalizados com *");
             return;
+        }
+
+
+        let regexTelefone = /^\(\d{2}\)\d{4,5}-\d{4}$/
+        let telefone = document.getElementById("telefone").value.toString()
+
+        if(!regexTelefone.test(telefone)){
+            carregando(false);
+            alert("Preencha o telefone corretamente: \n exemplo: (11)1111-1111 ou (11)11111-1111")
+            return
+        }
+
+        telefone = telefone.replace("-", "")
+        telefone = telefone.replace("(", "")
+        telefone = telefone.replace(")", "")
+
+        if(document.getElementById("complemento").value.length > 50){
+            carregando(false)
+            alert("O complemento deve ter no máximo 50 caracteres")
+            return
         }
 
         var cpfNumPassaporte = cpfPassaporte;
@@ -244,16 +336,27 @@ function FormularioDeReserva(props) {
                 </div>
 
                 <div id="divinterna">
-                    <label htmlFor="cidade">Cidade*:</label>
-                    <input id="cidade" className="inputdivisivel" type="text" required></input>
 
                     <label htmlFor="estado">Estado*:</label>
-                    <input id="estado" className="inputdivisivel" type="text" maxLength="2" required></input>
+                    <select id="estado" value = {ufSelecionada} onChange = {(event) => setUfSelecionada(event.target.value)} className="inputdivisivel" required>
+                        <option value = "">Selecione o Estado</option>
+                        {
+                            ufs.map(uf => <option key = {uf.sigla} value = {uf.sigla}>{uf.sigla}</option>)
+                        }
+                    </select>
+
+                    <label htmlFor="cidade">Cidade*:</label>
+                    <select id="cidade" value = {cidadeSelecionada} onChange = {(event) => setCidadeSelecionada(event.target.value)} className="inputdivisivel" required disabled>
+                        <option value="">Selecione a cidade</option>
+                        {
+                            cidades.map(cidade => <option key = {cidade.nome} value = {cidade.nome}>{cidade.nome}</option>)
+                        }
+                    </select>
                 </div>
 
                 <div id="divinterna">
                     <label htmlFor="telefone">Telefone*:</label>
-                    <input id="telefone" className="inputdivisivel" type="text" required></input> {/*Implementar máscara do input depois*/}
+                    <input id="telefone" onKeyPress = {mascararTelefone} className="inputdivisivel" type="text" required></input> {/*Implementar máscara do input depois*/}
                 </div>
 
                 <div id="carregando" className="nada"></div>
