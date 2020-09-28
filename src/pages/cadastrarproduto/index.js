9e1beff9343523d2548cad59f402ee1f0c2659f4
Gabriel <gabrielzambuzi7@gmail.com>
2020-09-28 15:06:55 -0300

    Término do cadastro de produto

--- src/pages/cadastrarproduto/index.js
+++ src/pages/cadastrarproduto/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import Cabecalho from '../../components/cabecalho'
 import InputMask from "react-input-mask";
+import requisicao from '../../functions/requisicao'
+import '../../components/Carregando.css'
 
 import "./styles.css"
 
@@ -9,7 +11,18 @@ const CadastrarProdutos = () => {
     const [nome, setNome] = useState("")
     const [custo, setCusto] = useState("")
 
-    function cadastrarProduto(){
+    const carregando = async (verdadeiro) => {
+        if (verdadeiro) {
+            document.getElementById("btnCadastrarProduto").className = "nada";
+            document.getElementById("carregando").className = "carregando";
+        }
+        else {
+            document.getElementById("btnCadastrarProduto").className = "";
+            document.getElementById("carregando").className = "nada";
+        }
+    }
+
+    async function cadastrarProduto(){
         
         if(nome.length === 0){
             alert("Preencha o nome corretamente")
@@ -19,6 +32,26 @@ const CadastrarProdutos = () => {
         if(custo.length === 0 || custo.includes("_")){
             alert("Preencha o custo corretamente, com os 4 digitos")
             return
+        }
+
+        try {
+            await carregando(true);
+            
+            const res = await requisicao.post("cadastroDeProduto", 'nome=' + nome +
+            '&custo=' + custo)
+
+            if (res.status === "Sucesso") {
+                await carregando(false);
+                alert("Produto cadastrado com sucesso!");
+            }
+            else{
+                alert("Erro ao cadastrar produto!\nErro: " + res.dados);
+                await carregando(false);
+            }
+
+        } catch (err) {
+            await carregando(false);
+            console.log(err);
         }
         
     }
@@ -39,7 +72,8 @@ const CadastrarProdutos = () => {
                     <p>Custo: R$</p>
                     <InputMask name = "custo" id = "custo" mask = "99.99" value = {custo} onChange = {e => setCusto(e.target.value)}/>
                 </div>
-                <button type = "button" onClick = {cadastrarProduto}>Cadastrar Produto</button>
+                <div id="carregando" className="nada"></div>
+                <button id="btnCadastrarProduto" type = "button" onClick = {cadastrarProduto}>Cadastrar Produto</button>
             </form>
         </div>
     )
