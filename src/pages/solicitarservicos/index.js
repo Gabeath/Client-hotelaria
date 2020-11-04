7eb24146a6edd4fd03a3cc761aa878805692f5c3
Angelo <angelo-sc1@hotmail.com>
2020-11-03 21:42:44 -0300

    continuação solicitar servicos

--- src/pages/solicitarservicos/index.js
+++ src/pages/solicitarservicos/index.js
@@ -15,12 +15,25 @@ const Servicos = () => {
     if(!logged)
         history.push('/loginHospede')
 
+    const [total, setTotal] = useState("")
     const [servicos, setServico] = useState([])
     const [carrinho, setCarrinho] = useState([])
+    const [atualizar, setAtualizar] = useState(false)
 
     useEffect(() => {
         requisicao.get('consultarServico').then(servicos => setServico(servicos.dados))
     }, [])
+
+    useEffect(() => {
+        let total = 0;
+        let qtdd;
+        carrinho.forEach(item => {
+            qtdd = document.getElementById(item.id).value
+            total += item.custo * qtdd
+        });
+        setTotal(total.toFixed(2))
+    }, 
+    [carrinho, atualizar])
 
     function addToCart(servico){
 
@@ -69,23 +82,33 @@ const Servicos = () => {
             <div className="container">
                 <div className="servicos">
                     <h1>Serviços disponíveis</h1>
-                    {servicos.map(servico => <div key = {servico.nome}>
-                        <div>{servico.nome}</div>
-                        <div>{servico.custo}</div>
-                        <button onClick = {() => addToCart(servico)}>+</button>
-                    </div>)}
+                    <div className = "content">
+                        {servicos.map(servico => <div className = "servico" key = {servico.nome}>
+                            <div><strong>Descrição: </strong>{servico.nome}</div>
+                            <div><strong>Custo: </strong>R$ {servico.custo}</div>
+                            <button onClick = {() => addToCart(servico)}>+</button>
+                        </div>)}
+                    </div>
                 </div>
 
                 <div className = "carrinho">
                     <h1>Carrinho</h1>
-                    {carrinho.map(item => <div key = {item.nome}>
-                        <div>{item.nome}</div>
-                        <div>{item.custo}</div>
-                        <label htmlFor={item.id}>Quantidade: </label>
-                        <input id = {item.id} type= "number" min = "1" defaultValue = "1"/>
-                        <button onClick = {() => removeToCart(item)}>X</button>
-                    </div>)}
-                    {carrinho.length > 0 && <button onClick = {solicitarServicos}>Solicitar Serviços</button>}
+                    {carrinho.length === 0 && <p>O carrinho está vazio.</p>}
+                    {
+                       carrinho.length > 0 && <div className = "itens">
+                            {carrinho.map(item => <div className = "item" key = {item.nome}>
+                                <div>{item.nome}</div>
+                                <div><strong>Custo: </strong>R$ {item.custo}</div>
+                                <label htmlFor={item.id}>Quantidade: </label>
+                                <input id = {item.id} type= "number" min = "1" defaultValue = "1" onChange = {() => setAtualizar(!atualizar)}/>
+                                <button title = "Remover do carrinho" onClick = {() => removeToCart(item)}>X</button>
+                            </div>)}
+                            {carrinho.length > 0 && <div><strong>Total: </strong>R$ {total}</div>}
+                            {carrinho.length > 0 && <button onClick = {solicitarServicos}>Solicitar Serviços</button>}
+                        </div>
+                    }
+                    
+                    
                 </div>
             </div>
         </div>
